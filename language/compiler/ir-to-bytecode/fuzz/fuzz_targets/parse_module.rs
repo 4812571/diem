d7efaac81584944e6e64ee350b2d25ef586719a6
Dylan <57853417+4812571@users.noreply.github.com>
2023-04-18 22:07:05 -0400

    [ci] integrated mayhem

---
+++ language/compiler/ir-to-bytecode/fuzz/fuzz_targets/parse_module.rs
@@ -0,0 +1,11 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use ir_to_bytecode::parser::parse_module;
+use move_symbol_pool::Symbol;
+
+fuzz_target!(|data: &[u8]| {
+    let source = String::from_utf8_lossy(data);
+    let symbol = Symbol::from("fuzz");
+    let _ = parse_module(symbol, &source);
+});
---
+++ language/compiler/ir-to-bytecode/fuzz/fuzz_targets/parse_script_or_module.rs
@@ -0,0 +1,11 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use ir_to_bytecode::parser::parse_script_or_module;
+use move_symbol_pool::Symbol;
+
+fuzz_target!(|data: &[u8]| {
+    let source = String::from_utf8_lossy(data);
+    let symbol = Symbol::from("fuzz");
+    let _ = parse_script_or_module(symbol, &source);
+});
